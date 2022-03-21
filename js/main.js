eaefbe462f5dc1fae4d0ff31b2714c8d439a131f
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-03-21 11:01:28 +0100

    add changes names

--- js/main.js
+++ js/main.js
@@ -12,19 +12,19 @@ list.classList.add('.js-list');
 //DATOS//
 
 const kittenImage1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
-const kittenName1 = "Anastacio";
+const kittenName1 = "anastacio";
 const kittenDesc1 =
   "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace1 = "British Shorthair";
 
 const kittenImage2 = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
-const kittenName2 = "Fiona";
+const kittenName2 = "fiona";
 const kittenDesc2 =
   "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace2 = "British Shorthair";
 
 const kittenImage3 = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
-const kittenName3 = "Cielo";
+const kittenName3 = "cielo";
 const kittenDesc3 =
   "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace3 = "British Shorthair";

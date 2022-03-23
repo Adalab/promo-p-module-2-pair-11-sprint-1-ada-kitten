1003b75c01a982f90131429912865677959805c2
andalons <andreacor001@gmail.com>
2022-03-23 11:17:39 +0100

    add events

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 "use strict";
-
-//constante para el formulario que se mostrará o no dependiendo del click:
-const newForm = document.querySelector('.js-new-form');
-//condicional que añade y quita la clase collapsed en función de si el formulario ya cuenta con esa clase o no
-if (newForm.classList.contains ("collapsed")) {
-  newForm.classList.remove ("collapsed");
-} else {
-  newForm.classList.add ("collapsed");
-}
-
-
+//primeros ejercicios (añadir clase)
 const list = document.querySelector ('.list');
-
 list.classList.add('.js-list');
+
+//formulario que se mostrará o no dependiendo del click:
+const newForm = document.querySelector('.js-new-form');
+const plusBtn = document.querySelector('.fa-plus-circle');
+
+plusBtn.addEventListener('click', ()=>{
+  newForm.classList.toggle ("collapsed");
+});
+
+
 
 //DATOS//
 
@@ -58,19 +57,64 @@ const kitten3 = `<li class="card">
 <p class="card_description">    ${kittenDesc3} </p>
 </article></li>` 
 
+list.innerHTML += kitten1;
+list.innerHTML += kitten2;
+list.innerHTML += kitten3;
 
-//condicional que muestra a los gatitos si en su descripción se muestra el valor "juguetón"
-const input_search_desc = document.querySelector('.js_in_search_desc');
-input_search_desc.value = 'juguetón';
-const descrSearchText = input_search_desc.value;
+/* EJERICIO 22/3: condicional que muestra a los gatitos si en su descripción se muestra el valor "juguetón"
+const inputSearchDesc = document.querySelector('.js_in_search_desc');
+inputSearchDesc.value = 'juguetón';
+const descrSearchText = inputSearchDesc.value;
 if( kittenDesc1.includes(descrSearchText) ) 
   { list.innerHTML += kitten1;}
 if( kittenDesc2.includes(descrSearchText) ) 
   { list.innerHTML += kitten2;}
 if( kittenDesc3.includes(descrSearchText) ) 
-  { list.innerHTML += kitten3;}
+  { list.innerHTML += kitten3;} */
 
   //Valida la información de las razas, si el gatito no tiene la raza especificada mostraremos el mensaje No se ha especificado la raza. Para ello verifica si la raza tienen algún valor, y en dependencia del resultado de la comprobación muestra un mensaje o la raza si existe.
 
   if (kittenRace3 === "") {
     list.innerHTML += `<h3 class='card_race'>No se ha especificado la raza</h3>`;} 
+
+    //FORMULARIO NUEVO GATITO: validar el caso de que la usuaria le dé clic al botón Añadir y no haya rellenado lo valores de la foto, nombre y descripción de manera obligatoria, la raza no es un campo obligatorio a insertar.
+
+    const addButton = document.querySelector('.js-btn-add');
+
+      const inputDesc = document.querySelector(".js-input-desc");
+      const inputPhoto = document.querySelector(".js-input-photo");
+      const inputName = document.querySelector(".js-input-name");
+      const inputRace = document.querySelector(".js-input-race");
+      const labelMesageError = document.querySelector(".js-label-error");
+
+    addButton.addEventListener('click', ()=>{
+      const valueDesc = inputDesc.value;
+      const valuePhoto = inputPhoto.value;
+      const valueName = inputName.value;
+      const valueRace = inputRace.value;
+      
+      if (valueDesc === "" && valuePhoto === "" && valueName === "" && valueRace === "") {
+        //completa el código
+        event.preventDefault();
+        console.log("hola");
+    
+      } else {
+        //completa el código
+        labelMesageError.innerHTML="Debe rellenar todos los valores";
+      }
+    });
+
+ //Validar formulario búsqueda. En este ejercicio vamos a validar el caso de que la usuaria le dé clic al botón Buscar y no haya rellenado alguno de los valores a buscar
+    const inputSearchDesc = document.querySelector('.js_in_search_desc');
+    const inputSearchRace = document.querySelector('.js_in_search_race');
+    const searchButton = document.querySelector('.js-search-btn')
+
+    searchButton.addEventListener('click', ()=>{
+      const valueDesc2 = inputSearchDesc.value;
+      const valueRace2 = inputSearchRace.value;
+      if (valueDesc2 === "" && valueRace2 === "") {
+        //completa el código
+        event.preventDefault();
+        console.log("hola");
+      }
+    });
